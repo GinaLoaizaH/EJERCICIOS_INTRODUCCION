0690498db76667dc6cdb8a21a0c74fea652f116a
GinaLoaizaH <ginita011295@gmail.com>
2024-04-19 08:38:24 -0500

    Realicé cambios en el ejercicio 9

--- src/Ejercicio9.java
+++ src/Ejercicio9.java
@@ -7,21 +7,27 @@ public class Ejercicio9 {
         Scanner teclado = new Scanner(System.in);
         double precioTotal, PRECIO_SILLA = 40000;
         int sillas;
+        String mensaje = "";
 
         System.out.println("Ingrese el número de sillas que desea comprar");
         sillas = teclado.nextInt();
 
         if (sillas > 39) {
             precioTotal = (sillas * PRECIO_SILLA) - (sillas * PRECIO_SILLA) * 0.3;
-            System.out.println("El total de su pedido con el 30% de descuento es: " + precioTotal);
+            mensaje = "30% de descuento ";
         } else if (sillas > 11) {
             precioTotal = (sillas * PRECIO_SILLA) - (sillas * PRECIO_SILLA) * 0.2;
-            System.out.println("El total de su pedido con el 20% de descuento es: " + precioTotal);
+            mensaje = "20% de descuento ";
         } else if (sillas > 4) {
             precioTotal = (sillas * PRECIO_SILLA) - (sillas * PRECIO_SILLA) * 0.1;
-            System.out.println("El total de su pedido con el 10% de descuento es: " + precioTotal);
-        } else
+            mensaje = "10% de descuento ";
+
+        } else {
             precioTotal = sillas * PRECIO_SILLA;
-        System.out.println("Su pedido no tiene descuento, el valor es: " + precioTotal);
+            System.out.println("Su pedido no tiene descuento, el valor es: " + precioTotal);
+        }
+
+        System.out.println("El total de su pedido con el " + mensaje + "es: " + precioTotal);
+
     }
 }
